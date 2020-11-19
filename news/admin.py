a595bbff3f24b1ecdafce01a5c287f0427ede426
rowenarono95 <rowenarono@gmail.com>
2020-11-19 16:39:33 +0300

    first commit

---
+++ news/admin.py
@@ -0,0 +1,11 @@
+
+from django.contrib import admin
+from .models import Article,tags
+
+# Register your models here.
+class ArticleAdmin(admin.ModelAdmin):
+    filter_horizontal =('tags',)
+
+
+admin.site.register(Article,ArticleAdmin)
+admin.site.register(tags)
---
+++ news/views.py
@@ -0,0 +1,79 @@
+ 
+from django.shortcuts import render,redirect
+from django.http import HttpResponse, Http404,HttpResponseRedirect
+import datetime as dt
+from .models import Article
+from .forms import NewArticleForm, NewsLetterForm
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+def news_today(request):
+    date = dt.date.today()
+    news = Article.todays_news()
+    
+    if request.method == 'POST':
+        form = NewsLetterForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['your_name']
+            email = form.cleaned_data['email']
+            recipient = NewsLetterRecipients(name = name,email =email)
+            recipient.save()
+            send_welcome_email(name,email)
+            HttpResponseRedirect('news_today')
+    else:
+        form = NewsLetterForm()
+    return render(request, 'all-news/today-news.html', {"date": date,"news":news,"letterForm":form}) 
+
+
+def past_days_news(request,past_date):
+    
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+        assert False
+
+    if date == dt.date.today():
+        return redirect(news_today)
+    
+    news = Article.days_news(date)
+    return render(request, 'all-news/past-news.html', {"date":date,"news":news})
+
+def search_results(request):
+
+    if 'article' in request.GET and request.GET["article"]:
+        search_term = request.GET.get("article")
+        searched_articles = Article.search_by_title(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'all-news/search.html',{"message":message,"articles": searched_articles})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'all-news/search.html',{"message":message})
+
+@login_required(login_url='/accounts/login/')
+def article(request,article_id):
+    try:
+        article = Article.objects.get(id = article_id)
+    except DoesNotExist:
+        raise Http404()
+    return render(request,"all-news/article.html", {"article":article})
+
+@login_required(login_url='/accounts/login/')
+def new_article(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewArticleForm(request.POST, request.FILES)
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.editor = current_user
+            article.save()
+        return redirect('newsToday')
+
+    else:
+        form = NewArticleForm()
+    return render(request, 'new_article.html', {"form": form})
+  
